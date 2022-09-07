b7b333629f13aea0503c3bc429e099c2c72a9cc0
Chonlasit-jui <prajehayarock@gmail.com>
2022-09-07 11:31:50 +0700

    9/7

--- App.js
+++ App.js
@@ -1,55 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import { Button } from 'react-native-web';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Button } from "react-native-web";
 
-function HomeScreen({navigation}){
-  return(
-    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-        <Text>Home!</Text>
-        <Button
-            title="GO TO SECOND"
-            onPress={() => {
-              navigation.navigate("Setting");
-            }}
-          />
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home!</Text>
+      <Button
+        title="GO TO SECOND"
+        onPress={() => {
+          navigation.navigate("Setting");
+        }}
+      />
     </View>
-  )
+  );
 }
 
-function SettingScreen({navigation}){
-  return(
-    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-        <Text>Settings!!</Text>
-        <Button
-            title="GO HOME"
-            onPress={() => {
-              navigation.navigate("Home");
-            }}
-          />
+function SettingScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings!!</Text>
+      <Button
+        title="GO HOME"
+        onPress={() => {
+          navigation.navigate("Home");
+        }}
+      />
     </View>
-  )
+  );
 }
 
 const Tab = createBottomTabNavigator();
 
-function MyTabs(){
-  return(
+function MyTabs() {
+  return (
     <Tab.Navigator>
-      <Tab.Screen name ="Home" component={HomeScreen}/>
-      <Tab.Screen name ="Setting" component={SettingScreen}/>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Setting" component={SettingScreen} />
     </Tab.Navigator>
-  )
+  );
 }
 
 export default function App() {
   return (
     <NavigationContainer>
-      <MyTabs/>
+      <MyTabs />
     </NavigationContainer>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
